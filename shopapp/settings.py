f9ccfabe61441019717e96590baa3926c1b9ab2d
YOUSSOUF <kouamou.noumejie@gmail.com>
2023-05-04 16:05:09 +0100

    add4

--- shopapp/settings.py
+++ shopapp/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-0114.up.railway.app']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Application definition
