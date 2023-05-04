97ae7a821be942dbd3cbc7693c7b8e3a48b60e67
YOUSSOUF <kouamou.noumejie@gmail.com>
2023-05-04 17:52:31 +0100

    add12

--- shopapp/settings.py
+++ shopapp/settings.py
@@ -147,4 +147,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
